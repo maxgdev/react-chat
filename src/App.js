b91062606b8ee593a7389a5fe8ba2d84cabde809
Max <25491453+maxgdev@users.noreply.github.com>
2023-01-21 11:15:01 +0000

    Setup ChatEngine component for API access-successful

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import chatEngine from './chat-engine';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>React Chat App with ChatEngine</h1>
+      <ChatEngine 
+        height="100vh"
+        projectID={chatEngine.projectID}
+        userName="Test1"
+        userSecret="testing#123"
+      
+      />
     </div>
   );
 }
